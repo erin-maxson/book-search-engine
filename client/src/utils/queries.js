f0a9e9a3285d6d9a047409f9ffaf5b9576311ad4
erin-maxson <erin@cohatch.com>
2022-10-14 17:35:19 -0700

    updating queries

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -6,8 +6,13 @@ export const GET_ME = gql`
       _id
       username
       email
-      bookCount
-      savedBooks
+      savedBooks{
+      bookIdimage
+      title
+      authors
+      description
+      link
+      }
     }
   }
 `;
